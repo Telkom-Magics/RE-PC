38b2b4a9f64acd5e39f47541fe86a248bd331ec4
mmdnando <76607918+mmdnando@users.noreply.github.com>
2021-02-04 16:51:54 +0800

    penempatan awal

---
+++ llll/Assets/script/mbscript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class mbscript : MonoBehaviour
+{
+    public Animation anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void mbtocase()
+    {
+        anim.Play("mbtocase");
+    }
+}
