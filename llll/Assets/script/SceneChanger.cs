9247826917d99164948f620965969342b9df9301
Muhammad Idri Junando <76607918+mmdnando@users.noreply.github.com>
2021-02-23 03:47:43 +0800

    implementasi animation trigred

---
+++ llll/Assets/script/SceneChanger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LoadScene(string scenee2)
+    {
+       SceneManager.LoadScene(scenee2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
